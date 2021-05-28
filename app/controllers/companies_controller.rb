2126893e1d75f40cf4b245ba1c31e5489d674302
kgw0422 <kgw660386@gmail.com>
2021-05-28 17:19:31 +0900

    顧客データ登録画面作成

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,9 +3,21 @@ class CompaniesController < ApplicationController
   end
 
   def new
-    @company = Company.all
+    @company = Company.new
   end
 
   def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:company_name, :president_name, :postal_code, :addresses, :phone_number)
   end
 end
